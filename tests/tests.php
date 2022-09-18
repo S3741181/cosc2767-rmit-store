833e08a01f671824b643c6ecd45a6a623103b884
Kirypoi <67997855+Kirypoi@users.noreply.github.com>
2022-09-18 14:52:01 +0700

    test withour composer

---
+++ tests/tests.php
@@ -0,0 +1,14 @@
+<?php
+    use PHPUnit\Framework\TestCase as TestCase;
+
+use function PHPUnit\Framework\assertEmpty;
+use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertNull;
+        
+    class DatabaseTest extends TestCase
+    {   
+        public function testConnection(){
+                $mysqui = new \MySQLi("localhost", "db_admin", "rmit_password", "rmit_store_db");
+                assertEmpty($mysqui -> connect_errno);
+        }
+    }
